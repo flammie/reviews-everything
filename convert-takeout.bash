b6946841e57a8e165b071a2ea8c49d8d20ac1acb
Flammie A Pirinen <flammie@iki.fi>
2025-08-08 01:55:05 +0200

    update takeout converter

--- convert-takeout.bash
+++ convert-takeout.bash
@@ -4,30 +4,29 @@
 #
 # Google Takeout json format as seen on 2022, https://takeout.google.com
 . review-funcs.bash
-set -x
 if test $# != 1 ; then
     echo "Usage: $0 GOOGLE-TAKEOUT-MAPS-REVIEWS.json"
     exit 1
 fi
 
-jq '.features[].properties.Location."Business Name" ' "$1" > gmaps.names
+jq '.features[].properties.location.name ' "$1" > gmaps.names
 while read -r line ; do
     name=$line
-    review=$(jq ".features[] | select(.properties.Location.\"Business Name\" ==
-    $name) | .properties.\"Review Comment\"" "$1")
-    stars=$(jq ".features[] | select(.properties.Location.\"Business Name\" ==
-    $name) | .properties.\"Star Rating\"" "$1" | head -n 1)
+    review=$(jq ".features[] | select(.properties.location.name == $name) |
+        .properties.review_text_published" "$1")
+    stars=$(jq ".features[] | select(.properties.location.name == $name) |
+        .properties.five_star_rating_published" "$1" | head -n 1)
     score=$((stars * 2))
-    lat=$(jq ".features[] | select(.properties.Location.\"Business Name\" ==
-    $name) | .properties.Location.\"Geo Coordinates\".Latitude" "$1")
-    long=$(jq ".features[] | select(.properties.Location.\"Business Name\" ==
-    $name) | .properties.Location.\"Geo Coordinates\".Longitude" "$1")
-    address=$(jq ".features[] | select(.properties.Location.\"Business Name\" ==
-    $name) | .properties.Location.Address" "$1")
-    google=$(jq ".features[] | select(.properties.Location.\"Business Name\" ==
-    $name) | .properties.\"Google Maps URL\"" "$1")
-    timing=$(jq ".features[] | select(.properties.Location.\"Business Name\" ==
-    $name) | .properties.Published" "$1")
+    lat=$(jq ".features[] | select(.properties.location.name == $name) |
+        .geometry.coordinates[1]" "$1")
+    long=$(jq ".features[] | select(.properties.location.name == $name) |
+        .geometry.coordinates[0]" "$1")
+    address=$(jq ".features[] | select(.properties.location.name == $name) |
+        .properties.location.address" "$1")
+    google=$(jq ".features[] | select(.properties.location.name == $name) |
+        .properties.google_maps_url" "$1")
+    timing=$(jq ".features[] | select(.properties.location.name == $name) |
+        .properties.date" "$1")
     outfile=docs/$(filenamify "${name//\"}").markdown
     echo "writing to $outfile"
     echo "---" > "$outfile"
@@ -37,15 +36,17 @@ while read -r line ; do
     echo "latitude: $lat" >> "$outfile"
     echo "longitude: $long" >> "$outfile"
     echo "---" >> "$outfile"
-    echo "$review" | fmt >> "$outfile"
+    echo "# Flammie reviews: ${name//\"/}" >> "$outfile"
     echo >> "$outfile"
-    echo "> parts of review were originally posted on [Google Maps page of " \
+    echo "$review" | sed -e 's/^"//' -e 's/"$//' | fmt >> "$outfile"
+    echo >> "$outfile"
+    echo "> parts of review were originally posted on [Google Maps page of" \
         >> "$outfile"
-    echo "  $name]($google)" >> "$outfile"
+    echo "  $name](${google//\"/})" >> "$outfile"
     echo "---" >> "$outfile"
-    echo "Score: $(score_stars $score)" >> "$outfile"
-    echo "Address: $address" >> "$outfile"
-    echo -n "Map location: [$long $lat" >> "$outfile"
+    echo "> *Score*: $(score_stars $score)" >> "$outfile"
+    echo "> *Address*: $address" >> "$outfile"
+    echo -n "> *Map location*: [$long $lat" >> "$outfile"
     echo "(OpenStreetMap)](https://www.openstreetmap.org/?mlat=$lat&mlon=$long&zoom=12)"    >> "$outfile"
 done <  gmaps.names
 
